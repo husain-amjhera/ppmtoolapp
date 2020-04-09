198a9dd3bd4be0d6ff0d741e00463caa8741ce4a
husain-amjhera <goofyhusain@gmail.com>
2020-04-09 16:15:28 +0530

    first commit

---
+++ ppmtoolapp/src/test/java/com/husain/ppmapi/PpmtoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husain.ppmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
